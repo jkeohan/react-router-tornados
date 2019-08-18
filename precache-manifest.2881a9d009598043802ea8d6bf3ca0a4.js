2fb9072a5e7f99213bd99f2fbc7919b5772a5939
Joe M. Keohan <joekeohan@Joes-MacBook-Pro-2.local>
2019-08-18 19:02:18 -0400

    Updates

---
+++ precache-manifest.2881a9d009598043802ea8d6bf3ca0a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c411aa51e71c5574bd55a38d60a3fa",
+    "url": "/react-router-tornados/index.html"
+  },
+  {
+    "revision": "b3b70bb445c689a647e2",
+    "url": "/react-router-tornados/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ec34ea3f6e99941dabf8",
+    "url": "/react-router-tornados/static/js/2.911baf65.chunk.js"
+  },
+  {
+    "revision": "b3b70bb445c689a647e2",
+    "url": "/react-router-tornados/static/js/main.8a45fa12.chunk.js"
+  },
+  {
+    "revision": "de1e194794104ce6ba49",
+    "url": "/react-router-tornados/static/js/runtime~main.415c603f.js"
+  }
+]);
